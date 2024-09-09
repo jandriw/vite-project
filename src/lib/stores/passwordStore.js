f69de4047d566061b854e472b738c427ade00b2b
jandriw <jan.barcos@gmail.com>
2024-09-09 06:18:22 +0200

    FC

--- src/lib/stores/passwordStore.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-// Inicializa stores independientes para la longitud y la contraseña
-export const passwordLengthStore = writable(6); // Store para la longitud de la contraseña
-export const passwordStore = writable([]); // Store para la contraseña
